0a69f2e7f1a18c562cfd12045349f5a109a22033
1009556415 <1009556415@qq.com>
2021-03-08 22:56:03 +0800

    修改bug

--- src/main/java/com/wangkang/gts4vect/App.java
+++ src/main/java/com/wangkang/gts4vect/App.java
@@ -3,6 +3,7 @@ package com.wangkang.gts4vect;
 
 import org.apache.log4j.Logger;
 import org.geotools.data.DataStore;
+import org.junit.Test;
 
 /**
  * @author wangkang
@@ -17,19 +18,38 @@ public class App {
         DataStore datastore = PGDatastore.getDefeaultDatastore();
         Geotools geotools = new Geotools(datastore);
         String geojsonpath = "C:\\test\\ChinaWorldCitysBigbelin\\chinaCompany2.geojson";
-        String shpfilepath = "C:\\test\\ChinaWorldCitysBigbelin\\MuchBig.shp";
-        String pgtableName = "MuchBigPolygon";
+        String shpfilepath = "E:\\Temp\\shap\\MuchBig.shp";
+        String pgtableName = "test";
 
 //        geotools.geojson2pgtable(geojsonpath, pgtableName);
-        geotools.geojson2shp(geojsonpath, shpfilepath);
+//        geotools.geojson2shp(geojsonpath, shpfilepath);
 //        geotools.shp2geojson(shpfilepath, geojsonpath);
 //        geotools.shp2pgtable(shpfilepath, pgtableName);
         utility.tagLast("shp导入postgis");
 
 //        geotools.pgtable2geojson(pgtableName, geojsonpath);
-//        geotools.pgtable2shp(pgtableName, shpfilepath, "geom");
+        geotools.pgtable2shp(pgtableName, shpfilepath, "geom");
 
 
+    }
+
+    @Test
+    public void importData(){
+        DataStore datastore = PGDatastore.getDefeaultDatastore();
+        Geotools geotools = new Geotools(datastore);
+        String geojsonpath = "D:\\git\\gts4vect\\src\\main\\java\\com\\wangkang\\gts4vect\\polygon.geojson";
+        String shpfilepath = "E:\\Temp\\shap\\MuchBig.shp";
+        String pgtableName = "test2";
+        geotools.geojson2pgtable(geojsonpath, pgtableName);
+    }
+
+    @Test
+    public void export(){
+        DataStore datastore = PGDatastore.getDefeaultDatastore();
+        Geotools geotools = new Geotools(datastore);
+        String shpfilepath = "E:\\Temp\\shap\\MuchBig.shp";
+        String pgtableName = "test2";
+        geotools.pgtable2shp(pgtableName, shpfilepath, "geom");
     }
 
     private static Logger logger = Logger.getLogger(App.class);
